847d8a38b3f8c300ae12fc300162a4c29dc53023
Jacob <cool_stewj@yahoo.com>
2018-03-27 13:40:14 -0500

    lightning symbols

--- simplesymbols.js
+++ simplesymbols.js
@@ -1,20 +1,8 @@
 const letterindexarray = "abcdefghijklmnopqrstuvwxyz"
 const uletterindexarray = letterindexarray.toUpperCase();
 function SimpleSymbols(str) {
-    let result = true;
     for (let i = 1; i < str.length-1; i++) {
-        if (letterindexarray[str[i].charCodeAt(0) - 97]) {
-            if (str[i-1] != '+' || str[i+1] != '+') {
-                result = false;
-                break;
-            }
-        }
-        else if (uletterindexarray[str[i].charCodeAt(0) - 65]) {
-            if (str[i-1] != '+' || str[i+1] != '+') {
-                result = false;
-                break;
-            }
-        }
+        if (letterindexarray[str[i].charCodeAt(0) - 97] || uletterindexarray[str[i].charCodeAt(0) - 65]) if (str[i-1] != '+' || str[i+1] != '+') return false;
     }
-    return result;
+    return true;
 }
